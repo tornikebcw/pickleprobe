8b98f666216f9f3c676cf727863e9a6520fa1861
tornike <tornike@bcw.group>
2023-06-19 20:38:56 +0400

    fixing formatter

--- main.py
+++ main.py
@@ -18,7 +18,9 @@ log = logging.getLogger('pickleLogger')
 
 if env == "prod":
     from systemd.journal import JournalHandler
-    log.addHandler(JournalHandler())
+    journald_handler = JournalHandler(SYSLOG_IDENTIFIER='pickleprobe')
+    log.addHandler(journald_handler)
+    log.setLevel(logging.INFO)
 else:
     stream_handler = logging.StreamHandler()
     log.addHandler(stream_handler)

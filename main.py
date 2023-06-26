612be656346452584fea39858171afb9d73df6b1
tornike <tornike@bcw.group>
2023-06-26 12:03:48 +0400

    added phantom opera service

--- main.py
+++ main.py
@@ -158,6 +158,12 @@ if client == 'eth2':
     for servicename in servicenames:
         schedule.every(15).seconds.do(
             lambda servicename=servicename: check_service(servicename))
+if client == 'opera':
+    servicenames = ['opera']
+    for servicename in servicenames:
+        schedule.every(15).seconds.do(
+            lambda servicename=servicename: check_service(servicename))
+
 else:
     log.info("Name for Go Client was not provided skipping service status check")
 
